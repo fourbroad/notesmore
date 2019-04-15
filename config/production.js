5d4508239a637faac74c39624a6223f190c97dff
fourbroad <zhusihong@ins24.com>
2019-04-15 16:45:39 +0800

    修改：     config/production.js

--- config/production.js
+++ config/production.js
@@ -1,6 +1,6 @@
 const config = {
   elasticSearch: {
-    host: 'es-cn-0pp11feta0014hcyr.elasticSearch.aliyuncs.com:9200',
+    host: 'es-cn-mp9136c4d000f4mll.elasticSearch.aliyuncs.com:9200',
     apiVersion: '6.3',
     requestTimeout: 120000
   },
