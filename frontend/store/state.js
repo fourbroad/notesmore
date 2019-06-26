54c1f6f53836c9f83b3f4ea25696f46e0781be6b
Max Fang <fangguixian@qq.com>
2019-06-26 09:05:31 +0800

    Update state.js

--- frontend/store/state.js
+++ frontend/store/state.js
@@ -2,92 +2,92 @@ import client from 'api/client'
 import jwtDecode from 'jwt-decode';
 
 export default {
-  get token() {
-    let token = localStorage.getItem('token')
-    if (!_.isEmpty(token)) {
-      let decodedToken = jwtDecode(token), time = new Date().getTime() / 1000;
-      if (time < decodedToken.exp) {
-        return token
-      }
-    }
-    return null
-  },
-  set token(value) {
-    if(_.isEmpty(value)){
-      localStorage.removeItem('token')
-    }else{
-      localStorage.setItem('token', value)      
-    }
-  },
+    get token() {
+        let token = localStorage.getItem('token')
+        if (!_.isEmpty(token)) {
+            let decodedToken = jwtDecode(token), time = new Date().getTime() / 1000;
+            if (time < decodedToken.exp) {
+                return token
+            }
+        }
+        return null
+    },
+    set token(value) {
+        if (_.isEmpty(value)) {
+            localStorage.removeItem('token')
+        } else {
+            localStorage.setItem('token', value)
+        }
+    },
 
-  get acgUrl(){
-    return localStorage.getItem('acgUrl')||'https://notesmore.com/domains';
-  },
-  set acgUrl(url){
-    localStorage.setItem('acgUrl', url);
-  },
+    get acgUrl() {
+        return localStorage.getItem('acgUrl') || 'http://localhost:3000/domains';
+    },
+    set acgUrl(url) {
+        localStorage.setItem('acgUrl', url);
+    },
 
-  get printApiMessage(){
-    return localStorage.getItem('printApiMessage');
-  },
+    get printApiMessage() {
+        return localStorage.getItem('printApiMessage');
+    },
 
-  set printApiMessage(show){
-    if(!show){
-      localStorage.removeItem('printApiMessage');  
-    }else{
-      localStorage.setItem('printApiMessage', true);
-    }
-  },
+    set printApiMessage(show) {
+        if (!show) {
+            localStorage.removeItem('printApiMessage');
+        } else {
+            localStorage.setItem('printApiMessage', true);
+        }
+    },
 
-  get currentDomainId() {
-    let domain = document.domain, currentDomainId, index
-    index = domain.indexOf('.notesmore.com');
-    if (index >= 0) {
-      currentDomainId = domain.slice(0, index);
-    }
+    get currentDomainId() {
+        let domain = document.domain, currentDomainId, index
+        index = domain.indexOf('.notesmore.com');
+        if (index >= 0) {
+            currentDomainId = domain.slice(0, index);
+        }
 
-    index = domain.indexOf('.notesmore.cn');
-    if (index >= 0) {
-      currentDomainId = domain.slice(0, index);
-    }
+        index = domain.indexOf('.notesmore.cn');
+        if (index >= 0) {
+            currentDomainId = domain.slice(0, index);
+        }
 
-    if (!currentDomainId || currentDomainId == 'www') {
-      currentDomainId = localStorage.getItem("currentDomainId") || '.root';
-    }
+        if (!currentDomainId || currentDomainId == 'www') {
+            currentDomainId = localStorage.getItem("currentDomainId") || '.root';
+        }
 
-    return currentDomainId;
-  },
+        return currentDomainId;
+    },
 
-  set currentDomainId(value) {
-    localStorage.setItem('currentDomainId', value)
-  },
+    set currentDomainId(value) {
+        localStorage.setItem('currentDomainId', value)
+    },
 
-  get favoritesOpened(){
-    return localStorage.getItem('favoritesOpened');
-  },
+    get favoritesOpened() {
+        return localStorage.getItem('favoritesOpened');
+    },
 
-  set favoritesOpened(opened){
-    if(!opened){
-      localStorage.removeItem('favoritesOpened');  
-    }else{
-      localStorage.setItem('favoritesOpened', true);
-    }
-  },
+    set favoritesOpened(opened) {
+        if (!opened) {
+            localStorage.removeItem('favoritesOpened');
+        } else {
+            localStorage.setItem('favoritesOpened', true);
+        }
+    },
 
-  get locale() {
-    return localStorage.getItem('locale') || (navigator.language == 'zh-CN' ? 'cn': 'en')
-  },
-  set locale(value) {
-    localStorage.setItem('locale', value)
-  },
-  
-  get isConnected(){
-    return client.isConnected();
-  },
-  currentUser: null,
-  favorites: [],
-  profile: null,
-  metas:[],
-  toasts: [],
-  isSidebarNavCollapse: false
+    get locale() {
+        return localStorage.getItem('locale') || (navigator.language == 'zh-CN' ? 'cn' : 'en')
+    },
+    set locale(value) {
+        localStorage.setItem('locale', value)
+    },
+
+    get isConnected() {
+        return client.isConnected();
+    },
+    currentUser: null,
+    favorites: [],
+    profile: null,
+    metas: [],
+    toasts: [],
+    isSidebarNavCollapse: false
 }
